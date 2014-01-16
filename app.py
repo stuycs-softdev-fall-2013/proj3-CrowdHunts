4eaf2b47decef6248cc1becc44bd046ad768a237
WFT <will.a.ft@gmail.com>
2014-01-16 13:15:07 -0500

    app.py entries for game html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,6 +6,15 @@ app = Flask(__name__)
 def index():
     return "Yo, it's an index"
 
+@app.route('/leaderboard')
+def leaders():
+    #db query
+    return render_template('leaderboard.html')
+
+@app.route('/u/<usern>')
+def profile(usern):
+    #db query
+    return render_template('user.html')
 
 if __name__ == "__main__":
     app.debug = True
