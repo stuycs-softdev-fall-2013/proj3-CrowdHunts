59126d7a22e21806392d8f61c2adbea41605ca4c
WFT <will.a.ft@gmail.com>
2014-01-26 22:06:57 -0500

    b64_combine

--- image_utils.py
+++ image_utils.py
@@ -5,7 +5,13 @@ import base64
 def combine_b64(top, bottom, x, y):
     a = b64_to_Image(top)
     b = b64_to_Image(bottom)
-    return combine(a, b, x, y)
+    return Image_to_b64(combine(a, b, x, y))
+
+def Image_to_b64(img):
+    ret = StringIO()
+    img.save(ret, img.format)
+    ret.seek(0)
+    return base64.b64encode(ret.getvalue())
 
 def b64_to_Image(b64_img):
     pic = StringIO()
