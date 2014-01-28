b7622dc2edb965b0c888ef3e0060f9346c80237b
Yaseen <yislam96@gmail.com>
2014-01-28 11:43:03 -0500

    added a home page

--- app.py
+++ app.py
@@ -13,6 +13,13 @@ def index():
     else:
         return redirect('/home')
 
+@app.route('/home')
+def home():
+    if not 'usern' in session:
+        return redirect('/')
+    else:
+        return render_template('home.html')
+
 # not very fun. we won't mention it, but we'll leave it there in case anyone's interested
 @app.route('/leaderboard')
 def leaders():
