6a3f0ced7a15da8bb5863611cfdf4b7957ce54f5
hunteriam <hhstuy@gmail.com>
2014-01-29 01:17:07 -0500

    bug fixes and shit

--- static/js/interface.js
+++ static/js/interface.js
@@ -11,10 +11,10 @@ function TouchUI() {
 	var UI = {
 		setFrame: function(context,frameNum) {
 			var oldFrame = -parseInt(context.style.left)/100
-			context.style.left = -100 * frameNum;
-			openFrame(context,oldFrame,frameNum);
+			context.style.left = -100 * frameNum + "%";
+			this.openFrame(context,oldFrame,frameNum);
 			return oldFrame;
-		}
+		},
 		openFrame: function(context,frameSource,frameNum) {
 			var frames = $(context).children('.frame[number='+frameNum+']');
 
