c9399643ec11ced2d20f9b41f95d3b81b5fbe511
WFT <will.a.ft@gmail.com>
2014-01-30 04:05:41 -0500

    dragging only on android

--- static/js/android.js
+++ static/js/android.js
@@ -1,13 +1,15 @@
 // env.streetview.setPov(h, p, z)
 // env.touchManager.touches.length
-$(document).on('drag', function(){
-    if (env.touchManager.touches.length == 2) {
-	var sview = env.streetView
-	var pov = sview.pano.getPov()
-	var heading = pov.heading
-	heading += event.detail.instantaneous.distance.x/5
-	var pitch = pov.pitch
-	pitch -= event.detail.instantaneous.distance.y/5
-	sview.setPov(heading, pitch, 1)
-    }
-})
+if (isMobile.Android()) {
+    $(document).on('drag', function(){
+	if (env.touchManager.touches.length == 2) {
+	    var sview = env.streetView
+	    var pov = sview.pano.getPov()
+	    var heading = pov.heading
+	    heading += event.detail.instantaneous.distance.x/5
+	    var pitch = pov.pitch
+	    pitch -= event.detail.instantaneous.distance.y/5
+	    sview.setPov(heading, pitch, 1)
+	}
+    })
+}
